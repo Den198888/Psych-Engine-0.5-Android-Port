5f878b1e6e6dfce2145da6386d9e3009b1feb6fc
Den198888 <96397208+Den198888@users.noreply.github.com>
2022-02-18 10:59:12 +0800

    Add files via upload

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -54,9 +54,6 @@ class HealthIcon extends FlxSprite
 					
 					animation.addByPrefix(char, 'Scott Icon', 23, true);
 					animation.play(char);
-					
-					offsetY = 12;
-					offsetX = -8;
 		default:
 			var name:String = 'icons/' + char;
 			if(!Paths.fileExists('images/' + name + '.png', IMAGE)) name = 'icons/icon-' + char; //Older versions of psych engine's support
@@ -78,7 +75,7 @@ class HealthIcon extends FlxSprite
 
 			updateHitbox();
 			if (char == 'scott') {
-				setGraphicSize(Std.int(width * 0.3));
+				setGraphicSize(Std.int(width * 1));
 				updateHitbox();
 			}
 			initialWidth = width;
